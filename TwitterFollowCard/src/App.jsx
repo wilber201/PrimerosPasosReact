dc99caec5cebe9c5bbe1f43a05f54cd8859606df
wilber201 <wilber201@hotmail.com>
2024-08-22 18:15:31 -0400

    Realizacion de estados
    -Actualizacion de vite.
    -Configurar el Localhost para que se conecten.
    -Agregacion de estado, Siguiendo - seguir.
    -Cambio de boton cuando esta siguiendo y cuando dando click
    -Uso de useState

--- TwitterFollowCard/src/App.jsx
+++ TwitterFollowCard/src/App.jsx
@@ -4,16 +4,12 @@ import { TwitterFollowCard } from "./TwitterFollowCard";
 export function App() {
 	return (
 		<>
-			<TwitterFollowCard _isFollowing={true} _userImage="wilber201" _username="@Wilber201">
+			<TwitterFollowCard userImage="wilber201" username="@Wilber201">
 				Wilber
 			</TwitterFollowCard>
 
-			<TwitterFollowCard _isFollowing={true} _userImage="midudev" _username="@midudev">
+			<TwitterFollowCard userImage="midudev" username="@midudev">
 				Midudev
-			</TwitterFollowCard>
-
-			<TwitterFollowCard _userImage="Guinxu" _username="@Guinxu">
-				Guinxu
 			</TwitterFollowCard>
 		</>
 	);
--- TwitterFollowCard/src/TwitterFollowCard.jsx
+++ TwitterFollowCard/src/TwitterFollowCard.jsx
@@ -1,18 +1,29 @@
-import { Children } from "react";
+import { useState } from "react";
 
-export function TwitterFollowCard({ children, _userImage, _username }) {
+export function TwitterFollowCard({ children, userImage, username }) {
+	const [isFollowing, setIsFollowing] = useState(false);
+
+	const text = isFollowing ? "Siguiendo" : "Seguir";
+	const followButtonClass = isFollowing ? "twitter-follow-btn isFollowing" : "twitter-follow-btn";
+
+	const handleClick = () => {
+		setIsFollowing(!isFollowing);
+	};
+
 	return (
 		<article className="twitter-card">
 			<header className="twitter-header">
-				<img src={`https://unavatar.io/${_userImage}`} alt="Wilber,Avatar" className="twitter-avatar" />
+				<img src={`https://unavatar.io/${userImage}`} alt="Wilber,Avatar" className="twitter-avatar" />
 				<div>
 					<strong>{children}</strong>
 					<br></br>
-					<span className="twitter-handle">{_username}</span>
+					<span className="twitter-handle">{username}</span>
 				</div>
 			</header>
 			<aside>
-				<button className="twitter-follow-btn">Follow</button>
+				<button className={followButtonClass} onClick={handleClick}>
+					{text}
+				</button>
 			</aside>
 		</article>
 	);
